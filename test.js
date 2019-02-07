320c0938d269e0a317bcf44a231a80fa5779c442
Khader <kakastarcris@gmail.com>
2019-02-07 09:46:16 +0200

    delete repeated tests Relates #24

--- test.js
+++ test.js
@@ -1,39 +1,38 @@
 const test = require('tape');
 const supertest = require('supertest');
-const router = require('./src/router'); 
+const router = require('./src/router');
 
 test('Home route returns a status code of 200', (t) => {
-    supertest(router)
+  supertest(router)
     .get('/')
     .expect(200)
-    .expect("content-type", /html/)
-    .end((err, res) => {
-        t.error(err);
-        t.equal(res.statusCode, 200, 'Should return status 200');
-        t.end();
+    .expect('content-type', /html/)
+    .end((err) => {
+      t.error(err);
+      t.end();
     });
 });
 
 test('loss  route', (t) => {
-    supertest(router)
+  supertest(router)
     .get('/hhhhjgfd')
     .expect(404)
     .expect('content-type', /html/)
     .end((err, res) => {
-        t.error(err);
-        t.equal(typeof(res),'object','should retun html file')
-        t.end();
+      t.error(err);
+      t.equal(typeof (res), 'object', 'should retun html file');
+      t.end();
     });
 });
 
 test('result route result', (t) => {
-    supertest(router)
+  supertest(router)
     .get('/result')
     .expect(200)
     .expect('content-type', /json/)
     .end((err, res) => {
-        t.error(err);
-        t.deepEqual(typeof res,'object', 'should return an object');
-        t.end();
+      t.error(err);
+      t.deepEqual(typeof res, 'object', 'should return an object');
+      t.end();
     });
 });
